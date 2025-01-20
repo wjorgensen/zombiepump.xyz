59c98d4166b6a906c9d3af97451250d51e06278e
Wezabis <wjorgensen29@gmail.com>
2025-01-20 00:59:25 -0600

    Initial commit with proper submodules

---
+++ contracts/src/ERC20-Bonding.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {ERC20} from "solady/tokens/ERC20.sol";
+
+contract ERC20_Bonding is ERC20 {
+    constructor() {
+        
+    }
+}
